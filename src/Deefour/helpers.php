1238d0173fac27f2587f6b8ebc96b5f69ca1c7ed
Jason Daly <jason@deefour.me>
2014-07-20 11:42:28 -0400

    avoiding php notices

--- src/Deefour/helpers.php
+++ src/Deefour/helpers.php
@@ -8,6 +8,18 @@
  */
 function getset($values) {
   foreach ($values as $value) {
+    // if an array is passed, check the key's existence (array position 1)
+    // in the source (array position 0) to avoid PHP notices.
+    if (is_array($value)) {
+      list($source, $key)  = $value;
+
+      if ( ! array_key_exists($key, $source)) {
+        continue;
+      }
+
+      return $source[$key];
+    }
+
     if ($value !== null) {
       return $value;
     }
